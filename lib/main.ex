8fd100b2f4050eb961fd4fa768414b579df50591
milindjayan <41206597+milindjayan@users.noreply.github.com>
2019-11-25 17:45:48 -0500

    Changed client user id name to :user<user number>

--- lib/main.ex
+++ lib/main.ex
@@ -1,13 +1,12 @@
 defmodule Twitter.Main do
 
-  def create_users(0,_nTweets) do
+  def create_users(0,_nTweets,_isOnline) do
      "Created all the users"
   end
 
-  def create_users(numUsers,nTweets) do
+  def create_users(numUsers,nTweets,isOnline) do
     userId =  numUsers
-
-    Twitter.Client.sign_up(userId,nTweets)
-    create_users(numUsers-1,nTweets)
+    Twitter.Client.sign_up(userId,nTweets,isOnline)
+    create_users(numUsers-1,nTweets,isOnline)
   end
 end
